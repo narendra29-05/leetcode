6dbd17420817e727de0ab7aee1cb79039d9a7316
kosireddy Narendra <22mh1a42a5@acoe.edu.in>
2025-11-09 19:06:32 +0530

    Time: 0 ms (100%), Space: 17.8 MB (31.47%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,15 +1,15 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-
         l=0
-        r=x//2+1
-        ans=1
+        r=x//2+1 #---->because the square root value always less then x//2+1 
+        
         while l<=r:
             m=(l+r)//2
             if m*m==x:
                 return m
-            if m*m<=x:
-                l=m+1
-            else:
+            if m*m>x:
                 r=m-1
+            if m*m<x:
+                l=m+1
         return l-1
+        
