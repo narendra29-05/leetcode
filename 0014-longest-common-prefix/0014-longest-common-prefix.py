fa54daada1ba607437e8ae4887f54831ca36a143
kosireddy Narendra <22mh1a42a5@acoe.edu.in>
2026-01-10 18:24:26 +0530

    Time: 0 ms (100%), Space: 19.6 MB (5.14%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,33 +1,22 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        class TrieNode:
-            def __init__(self):
-                self.children={}
-                self.end=False
-        class Trie:
-            def __init__(self):
-                self.root=TrieNode()
-            def insert(self,word):
-                node=self.root
-                for ch in word:
-                    if ch not in node.children:
-                        node.children[ch]=TrieNode()
-                    node=node.children[ch]
-                node.end=True
-            def search(self,word):
-                node=self.root
-                prefix=""
-                for ch in (word):
-                    if ch in node.children and len(node.children)==1 and not node.end:
-                        node=node.children[ch]
-                        prefix+=ch
-                    else:
-                        break
-                return prefix
-        trie=Trie()
-        for i in strs:
-            trie.insert(i)
-        
-        return trie.search(strs[0])
-                    
-            
+        if not strs:
+            return ""
+        ans=strs[0] #----->flower
+        final_ans=""
+        for string in strs[1:]:
+            dup=string  #--flight
+            if len(string)>=len(ans):  
+                dup=ans
+            final_ans=""
+            for i in range(len(dup)):
+                if string[i]==ans[i]:
+                    final_ans+=string[i]
+                else:
+                    break
+            print(final_ans)
+            ans=final_ans  #--->flow
+        return ans
+
+
+
